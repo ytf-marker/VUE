444919a1a6e3e9ed7288ba4f6e9ee52a88968650
wingoflights2 <wingoflights2@sina.com>
2020-01-10 17:46:16 +0800

    学习vueX 2020-01-10

---
+++ learnvuex/src/components/helloVuex.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>   
+        <h1>hello vueX</h1>
+            {{$store.state.counter}}
+            {{$store.getters.more20stu}} 
+    </div>
+</template>
+<script>
+export default {
+     
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ learnvuex/src/store/mutation-types.js
@@ -0,0 +1,4 @@
+export const INCREMENT = 'increment'
+export const DECREMENT = 'decrement'
+export const INCREMENTCOUNT = 'incrementCount'
+export const CHANGEINFO='chageinfo'
