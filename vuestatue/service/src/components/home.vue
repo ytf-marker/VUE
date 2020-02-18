fc77f7a5647856ff5bf329106b3f94e39df31959
ytf-marker <ytf101410@163.com>
2020-02-18 14:52:25 +0800

    学习axios的使用

--- vuestatue/service/src/components/home.vue
+++ vuestatue/service/src/components/home.vue
@@ -5,9 +5,19 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-
-}
+  
+ created(){
+   axios.defaults.baseURL='http://106.54.54.237:8000'  //全局端口
+axios.defaults.timeout= 5000   //超时时间
+   axios({
+      url:'/api/hy/home/multidata'
+   }).then(res=>{
+     console.log(res) 
+   })
+ }
+} 
 </script>
 
 <style>
--- vuestatue/service/src/main.js
+++ vuestatue/service/src/main.js
@@ -9,12 +9,12 @@ new Vue({
 }).$mount('#app')
 
 //1.axios的基本使用
-axios({
-  url:'http://106.54.54.237:8000/api/hy/home/multidata',
-  method:'get'
-}).then((res)=>{
-  console.log(res)
-})
+// axios({
+//   url:'http://106.54.54.237:8000/api/hy/home/multidata',
+//   method:'get'
+// }).then((res)=>{
+//   console.log(res)
+// })
 
 // axios({
 //   url:'http://106.54.54.237:8000/api/hy/home/data',
@@ -26,19 +26,36 @@ axios({
 // }).then((res)=>{
 //   console.log(res)
 // })
-
-
+//axiso的全局配置
+axios.defaults.baseURL='http://106.54.54.237:8000'  //全局端口
+axios.defaults.timeout= 5000   //超时时间
 //2.axios的并发请求
 
-axios.all([axios({
-  url:'http://106.54.54.237:8000/api/hy/home/data',
-  params:{
-    type:'sell',
-    page:5
-  }
-}),axios({
-  url:'http://106.54.54.237:8000/api/hy/home/multidata'
-}
-)]).then(res =>{
-console.log(res)
-})
+
+// axios.all([axios({
+//   url:'/api/hy/home/data',
+//   params:{
+//     type:'sell',
+//     page:5
+//   }
+// }),axios({
+//   url:'/api/hy/home/multidata'
+// }
+// )]).then(axios.spread((res1,res2)=>{
+//   console.log(res1);
+//   console.log(res2);
+// }))   
+
+//4.0创建对应的axios实例(为了应对一个项目里面有多个根路径)
+
+// const instancel = axios.create({
+//   baseURL:'http://106.54.54.237:8000',
+//   timeout:5000
+// })
+
+// instancel({
+//   url:'/home/multidata'
+// }).then((res)=>{
+//   console.log(res['data']) 
+// })
+
